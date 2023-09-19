d9e08a6150a8a3d012093631c2d8e531a55f8542
Md Rahim Sarker <61264159+littlesarker@users.noreply.github.com>
2023-09-19 17:23:26 +0600

    Add files via upload

---
+++ Tab_Bar/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(const TabBarDemo ());
+}
+
+class TabBarDemo extends StatelessWidget{
+  const  TabBarDemo({super.key});
+
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.home)),
+                Tab(icon: Icon(Icons.settings)),
+                Tab(icon: Icon(Icons.directions_bike)),
+
+              ],
+            ),
+            title: const Text('Tabs Demo'),
+          ),
+
+          body: const TabBarView(
+            children: [
+              Icon(Icons.home),
+              Icon(Icons.settings),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
