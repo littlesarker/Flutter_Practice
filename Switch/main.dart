021c1b88a73484cbd0bd05424117c0ce9e343ac1
Md Rahim Sarker <61264159+littlesarker@users.noreply.github.com>
2023-09-20 16:12:58 +0600

    Used to toggle the on/off state of a single setting.

---
+++ Switch/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const SwitchApp());
+
+class SwitchApp extends StatelessWidget {
+  const SwitchApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Switch sample')),
+        body: const Center(
+          child: SwitchExample(),
+        ),
+      ),
+    );
+  }
+}
+
+class SwitchExample extends StatefulWidget {
+  const SwitchExample({super.key});
+
+  @override
+  State<SwitchExample> createState() => _SwitchExampleState();
+}
+
+class _SwitchExampleState extends State<SwitchExample> {
+  bool light = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+        value: light,
+        activeColor: Colors.red,
+        onChanged: (bool value) {
+          // This is called when the user toggles the switch.
+          setState(() {
+            light = value;
+          });
+        });
+    }
+  }
